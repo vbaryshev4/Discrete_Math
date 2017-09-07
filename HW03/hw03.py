a7f8c1cac9f20388a750c99ecc90b682730b1b67
Vladimir Baryshev <vbaryashev@gmail.com>
2017-09-07 13:48:44 +0300

    HW03 added

--- HW03/hw03.py
+++ HW03/hw03.py
@@ -6,11 +6,31 @@
 # (x+y)%3 
 # x/y
 
+# Конструктор итерратора – [элмент, итерратор, условие]
+
 A={1,2,3,4,5,6,7,8,9} 
 B={1,2,3,4,5,6,7,8,9}
 
 R1 = [[True if (x+y)%3==0 else False for y in A]for x in B]
 print(R1)
+# [[False, True, False, False, True, False, False, True, False], 
+#  [True, False, False, True, False, False, True, False, False], 
+#  [False, False, True, False, False, True, False, False, True], 
+#  [False, True, False, False, True, False, False, True, False], 
+#  [True, False, False, True, False, False, True, False, False], 
+#  [False, False, True, False, False, True, False, False, True], 
+#  [False, True, False, False, True, False, False, True, False], 
+#  [True, False, False, True, False, False, True, False, False], 
+#  [False, False, True, False, False, True, False, False, True]]
+
 R2 = [[True if x%y==0 else False for y in A]for x in B]
 print(R2)
-# [элмент, итерратор, условие]
+# [[True, False, False, False, False, False, False, False, False],
+#  [True, True, False, False, False, False, False, False, False], 
+#  [True, False, True, False, False, False, False, False, False], 
+#  [True, True, False, True, False, False, False, False, False], 
+#  [True, False, False, False, True, False, False, False, False], 
+#  [True, True, True, False, False, True, False, False, False], 
+#  [True, False, False, False, False, False, True, False, False], 
+#  [True, True, False, True, False, False, False, True, False], 
+#  [True, False, True, False, False, False, False, False, True]]
